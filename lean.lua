b82bc2badb1014fc0e83a3da75f0fd9e71f048c7
Graham Leach-Krouse <gleachkr@gmail.com>
2023-02-22 14:34:31 -0600

    Add commonmark parsing

--- lean.lua
+++ lean.lua
@@ -4,10 +4,14 @@ local commentstart = lpeg.P([[/-]]) * nonwhitespace
 
 local commentend = lpeg.P([[-/]])
 
+function HandleComment(raw)
+    return table.unpack(pandoc.read(raw,"commonmark").blocks)
+end
+
 local G = lpeg.P{
     "Pandoc";
     Pandoc = lpeg.Ct((lpeg.V"block" + lpeg.V"comment")^0) / pandoc.Pandoc,
-    comment = commentstart * lpeg.C((1 - commentend)^1) * commentend / pandoc.Para,
+    comment = commentstart * lpeg.C((1 - commentend)^1) * commentend / HandleComment,
     block = lpeg.C((1 - commentstart)^1) / pandoc.CodeBlock
 }
 
