585a23551a4a497fa255683f126cc816965527cd
Graham Leach-Krouse <gleachkr@gmail.com>
2023-04-12 14:34:18 -0500

    Add support for specifying an alternative pandoc reader

--- lean.lua
+++ lean.lua
@@ -1,17 +1,19 @@
-local nonwhitespace = (1 - lpeg.P(" "))^0
+local nonwhitespace = (1 - lpeg.S("\n\r\t "))^0
 
-local commentstart = lpeg.P([[/-]]) * nonwhitespace
+local commentstart = lpeg.P([[/-]])
 
 local commentend = lpeg.P([[-/]])
 
-function HandleComment(raw)
-    return table.unpack(pandoc.read(raw,"commonmark").blocks)
+function HandleComment(prefix,raw)
+    local dialect = "commonmark"
+    if prefix ~= nil then dialect = prefix end
+    return table.unpack(pandoc.read(raw,dialect).blocks)
 end
 
 local G = lpeg.P{
     "Pandoc";
     Pandoc = lpeg.Ct((lpeg.V"block" + lpeg.V"comment")^0) / pandoc.Pandoc,
-    comment = commentstart * lpeg.C((1 - commentend)^1) * commentend / HandleComment,
+    comment = commentstart * lpeg.C(nonwhitespace) * lpeg.C((1 - commentend)^1) * commentend / HandleComment,
     block = lpeg.C((1 - commentstart)^1) / pandoc.CodeBlock
 }
 
