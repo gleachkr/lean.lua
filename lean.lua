d87fb6241fbef3dfa0102ce94840908f05621538
Graham <gleachkr@gmail.com>
2025-01-04 17:49:31 -0500

    Ignore certain prefixes

--- lean.lua
+++ lean.lua
@@ -1,23 +1,30 @@
-lpeg = require("lpeg")
+local lpeg = require("lpeg")
 
-local nonwhitespace = (1 - lpeg.S("\n\r\t "))^0
+local nonwhitespace = (1 - lpeg.S("\n\r\t ")) ^ 0
 
 local commentstart = lpeg.P([[/-]])
 
 local commentend = lpeg.P([[-/]])
 
-function HandleComment(prefix,raw)
-    local dialect = "commonmark"
-    if prefix ~= "" then dialect = prefix end
-    return table.unpack(pandoc.read(raw,dialect).blocks)
+local ignorePrefix = {
+    ["!"] = true,
+    ["-"] = true,
+    [""] = true
+}
+
+function HandleComment(prefix, raw)
+    if ignorePrefix[prefix] then prefix = "commonmark" end
+    return table.unpack(pandoc.read(raw, prefix).blocks)
 end
 
-local G = lpeg.P{
-    "Pandoc";
-    Pandoc = lpeg.Ct((lpeg.V"block" + lpeg.V"comment")^0) / pandoc.Pandoc,
-    comment = commentstart * lpeg.C(nonwhitespace) * lpeg.C((1 - commentend)^1) * commentend / HandleComment,
-    block = lpeg.C((1 - commentstart)^1) / pandoc.CodeBlock
+local theGrammar = {
+    "Pandoc",
+    Pandoc = lpeg.Ct((lpeg.V "block" + lpeg.V "comment") ^ 0) / pandoc.Pandoc,
+    comment = commentstart * lpeg.C(nonwhitespace) * lpeg.C((1 - commentend) ^ 1) * commentend / HandleComment,
+    block = lpeg.C((1 - commentstart) ^ 1) / pandoc.CodeBlock
 }
+
+local G = lpeg.P(theGrammar)
 
 function Reader(input)
     return lpeg.match(G, tostring(input))
