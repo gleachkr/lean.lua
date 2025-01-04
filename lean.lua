8ba40fe2e80ffe73b39d98cb577162cf9bae1dee
Graham <gleachkr@gmail.com>
2025-01-04 17:55:49 -0500

    Skip empty code blocks

--- lean.lua
+++ lean.lua
@@ -17,11 +17,16 @@ function HandleComment(prefix, raw)
     return table.unpack(pandoc.read(raw, prefix).blocks)
 end
 
+function HandleCode(raw)
+    if raw:find("^[%s%c]+$") then return nil end
+    return pandoc.CodeBlock(raw)
+end
+
 local theGrammar = {
     "Pandoc",
     Pandoc = lpeg.Ct((lpeg.V "block" + lpeg.V "comment") ^ 0) / pandoc.Pandoc,
     comment = commentstart * lpeg.C(nonwhitespace) * lpeg.C((1 - commentend) ^ 1) * commentend / HandleComment,
-    block = lpeg.C((1 - commentstart) ^ 1) / pandoc.CodeBlock
+    block = lpeg.C((1 - commentstart) ^ 1) / HandleCode
 }
 
 local G = lpeg.P(theGrammar)
